8d064a2c5123e5ce32e88ba900d67ee24b981067
war2510 <151764276+war2510@users.noreply.github.com>
2023-12-17 18:10:19 +0300

    Решили все 4 задачи

--- Program.cs
+++ Program.cs
@@ -66,3 +66,103 @@
 
 
 //  Задача 3: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+//  {
+        // Создаем прямоугольный двумерный массив (например, 3x4)
+        // int[,] array = new int[,]
+        // {
+        //     { 1, 2, 3, 4 },
+        //     { 5, 6, 7, 8 },
+        //     { 9, 10, 11, 12 }
+        // };
+
+        // Находим строку с наименьшей суммой элементов
+    //     int minSum = int.MaxValue;
+    //     int minSumRowIndex = -1;
+
+    //     for (int row = 0; row < array.GetLength(0); row++)
+    //     {
+    //         int rowSum = 0;
+    //         for (int col = 0; col < array.GetLength(1); col++)
+    //         {
+    //             rowSum += array[row, col];
+    //         }
+
+    //         if (rowSum < minSum)
+    //         {
+    //             minSum = rowSum;
+    //             minSumRowIndex = row;
+    //         }
+    //     }
+
+    //     if (minSumRowIndex != -1)
+    //     {
+    //         Console.WriteLine($"Строка с наименьшей суммой элементов: {minSumRowIndex}");
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Массив пуст.");
+    //     }
+    // }
+
+
+    // Задача 4*(не обязательная): Задайте двумерный массив из целых чисел. Напишите программу, которая удалит строку и столбец, на пересечении которых расположен наименьший элемент массива. Под удалением понимается создание нового двумерного массива без строки и столбца
+
+    // {
+        // Создаем двумерный массив (например, 3x3)
+        // int[,] array = new int[,]
+        // {
+        //     { 1, 2, 3 },
+        //     { 4, 5, 6 },
+        //     { 7, 8, 9 }
+        // };
+
+        // // Находим индексы наименьшего элемента
+        // int minElement = int.MaxValue;
+        // int minRow = -1;
+        // int minColumn = -1;
+
+        // for (int row = 0; row < array.GetLength(0); row++)
+        // {
+        //     for (int col = 0; col < array.GetLength(1); col++)
+        //     {
+        //         if (array[row, col] < minElement)
+        //         {
+        //             minElement = array[row, col];
+        //             minRow = row;
+        //             minColumn = col;
+        //         }
+        //     }
+        // }
+
+        // Создаем новый массив без строки и столбца с наименьшим элементом
+        // int[,] trimmedArray = new int[array.GetLength(0) - 1, array.GetLength(1) - 1];
+        // int newRow = 0;
+
+        // for (int row = 0; row < array.GetLength(0); row++)
+        // {
+        //     if (row == minRow)
+        //         continue;
+
+        //     int newColumn = 0;
+        //     for (int col = 0; col < array.GetLength(1); col++)
+        //     {
+        //         if (col == minColumn)
+        //             continue;
+
+        //         trimmedArray[newRow, newColumn] = array[row, col];
+        //         newColumn++;
+        //     }
+        //     newRow++;
+        // }
+
+        // Выводим новый массив
+    //     Console.WriteLine("Новый массив без строки и столбца с наименьшим элементом:");
+    //     for (int row = 0; row < trimmedArray.GetLength(0); row++)
+    //     {
+    //         for (int col = 0; col < trimmedArray.GetLength(1); col++)
+    //         {
+    //             Console.Write(trimmedArray[row, col] + " ");
+    //         }
+    //         Console.WriteLine();
+    //     }
+    // }
--- obj/Debug/net8.0/DZ_Seminar_5.AssemblyInfo.cs
+++ obj/Debug/net8.0/DZ_Seminar_5.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ_Seminar_5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2172001facff2456cf08bbbc28d68d05366dcc5f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a552c3cac173c250f8563e0b9a00ad8fc58d90f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ_Seminar_5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ_Seminar_5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
